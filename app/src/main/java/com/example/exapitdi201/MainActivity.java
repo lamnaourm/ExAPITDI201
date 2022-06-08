8900f86ee24573144aba789c3aea88a60f60f599
lamnaourm <mohammed.lamnaour@gmail.com>
2022-06-08 14:55:07 +0100

    Initial commit

---
+++ app/src/main/java/com/example/exapitdi201/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.exapitdi201;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView t;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        t = findViewById(R.id.t);
+        ReadAPiPays p = new ReadAPiPays();
+        p.execute("https://gorest.co.in/public/v2/users");
+
+
+    }
+
+
+    public void methode(int...par){
+
+    }
+
+    class ReadAPiPays extends AsyncTask<String, Void,String> {
+
+        ProgressDialog progress;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progress = new ProgressDialog(MainActivity.this);
+            progress.setMessage("En cours de chargement");
+            progress.setCancelable(false);
+            progress.show();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            progress.dismiss();
+
+            String res = "";
+            try {
+                JSONArray arr = new JSONArray(s);
+
+                for(int i=0;i<arr.length();i++){
+                    res += arr.getJSONObject(i).getString("name") + "\n";
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            t.setText(res);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuilder res=new StringBuilder();
+            try {
+                String url = strings[0];
+                URL Myurl = new URL(url);
+                HttpURLConnection connection = (HttpURLConnection)Myurl.openConnection();
+
+                InputStreamReader input = new InputStreamReader(connection.getInputStream());
+                BufferedReader buffer = new BufferedReader(input);
+                String line="";
+
+                while((line=buffer.readLine())!=null){
+                    res.append(line);
+                }
+
+                input.close();
+                buffer.close();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return res.toString();
+        }
+    }
+}
